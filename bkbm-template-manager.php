81ce6914d3d590e51acec3383ddfe87a5d882efd
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-07-28 11:12:04 +0600

    update installer code

--- bkbm-template-manager.php
+++ bkbm-template-manager.php
@@ -5,8 +5,8 @@
  * Plugin URI: https://1.envato.market/bkbm-wp
  * Description: Templify KB - Knowledge Base Addon allows you to display Knowledge Base categories, tags and single posts  in custom templates without modifying any of the files inside theme forlder. Addon automatically handle BWL Knowledge base categories, tags and single posts templates. Addon comes with responsive and mobile friendly bkbm-grid layout. So that you can easily display you're Knowledge Base contents in small devices.
  * Author: Md Mahbub Alam Khan
- * Version: 1.1.3
- * Author URI: https://1.envato.market/bkbm-wp
+ * Version: 1.1.4
+ * Author URI: https://bluewindlab.net
  * WP Requires at least: 6.0+
  * Text Domain: bkb_tpl
  */
@@ -38,13 +38,15 @@ if (!class_exists('BKBM_Template_Manager')) {
 
                 define('BKBM_BOOTSTRAP_FRAMEWORK', (isset($bkb_data['bkb_tpl_bootstrap_status']) && $bkb_data['bkb_tpl_bootstrap_status'] == 1)  ? 1 : 0);
 
-                define("BWL_KB_TPL_PLUGIN_VERSION", '1.1.3'); // Addon version.
+                define("BWL_KB_TPL_PLUGIN_VERSION", '1.1.4'); // Addon version.
                 define('BKBTPL_PARENT_PLUGIN_INSTALLED_VERSION', get_option('bwl_kb_plugin_version')); // 
                 define('BKBTPL_ADDON_PARENT_PLUGIN_TITLE', '<b>BWL Knowledge Base Manager Plugin</b> ');
                 define('BKBTPL_ADDON_TITLE', '<b>Templify KB</b>');
                 define('BKBTPL_PARENT_PLUGIN_REQUIRED_VERSION', '1.4.2'); // change plugin required version in here.
                 define('BKBTPL_ADDON_CURRENT_VERSION', BWL_KB_TPL_PLUGIN_VERSION); // change plugin current version in here.
                 define('BKBTPL_ADDON_UPDATER_SLUG', plugin_basename(__FILE__)); // change plugin current version in here.
+
+                define("BKBTPL_ADDON_CC_ID", "11888104"); // Plugin codecanyon Id.
 
                 add_action('admin_notices', array($this, 'bkb_tpl_version_update_admin_notice'));
 
@@ -321,7 +323,7 @@ if (!class_exists('BKBM_Template_Manager')) {
                 'bkbm-tpl-admin',
                 'BkbmTplAdminData',
                 [
-                    'product_id' => 11888104,
+                    'product_id' => BKBTPL_ADDON_CC_ID,
                     'installation' => get_option('bkbm_tpl_installation')
                 ]
             );
