d176398f4152ad67b35e7b109376438ee6eec7a3
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-03-12 21:03:17 +0200

    removed the h1 tag from the title and add h2 tag

--- template/single/tpl-single-content-part.php
+++ template/single/tpl-single-content-part.php
@@ -15,44 +15,44 @@ if ($bkb_single_tpl_layout == 2) {
 
 <?php while (have_posts()) : the_post(); ?>
 
-    <header class="bkbm-single-header">
+<header class="bkbm-single-header">
 
-        <?php echo do_shortcode('[bkbm_tpl_bc /]'); ?>
+  <?php echo do_shortcode('[bkbm_tpl_bc /]'); ?>
 
-        <?php
+  <?php
 
         if (!post_password_required() && has_post_thumbnail()) :
 
         ?>
 
-            <div class="bkbm-featured-img-container">
+  <div class="bkbm-featured-img-container">
 
-                <?php
+    <?php
                 the_post_thumbnail();
                 ?>
 
-            </div> <!--  end .bkbm-featured-image-container  -->
+  </div> <!--  end .bkbm-featured-image-container  -->
 
-        <?php endif; ?>
+  <?php endif; ?>
 
 
-        <h1 class="bkbm-single-title"><?php the_title(); ?></h1>
+  <h2 class="bkbm-single-title"><?php the_title(); ?></h2>
 
-        <?php if (comments_open()) : ?>
-            <div class="comments-link">
-                <?php comments_popup_link('<span class="leave-reply">' . __('Leave a reply', 'bkb_tpl') . '</span>', __('1 Reply', 'bkb_tpl'), __('% Replies', 'bkb_tpl')); ?>
-            </div><!-- .comments-link -->
+  <?php if (comments_open()) : ?>
+  <div class="comments-link">
+    <?php comments_popup_link('<span class="leave-reply">' . __('Leave a reply', 'bkb_tpl') . '</span>', __('1 Reply', 'bkb_tpl'), __('% Replies', 'bkb_tpl')); ?>
+  </div><!-- .comments-link -->
 
-        <?php endif; // comments_open()   
+  <?php endif; // comments_open()   
         ?>
-    </header><!-- .entry-header -->
+</header><!-- .entry-header -->
 
-    <div class="bkbm-entry-content">
-        <?php the_content(__('Continue reading', 'bkb_tpl')); ?>
-        <?php wp_link_pages(array('before' => '<div class="page-links">' . __('Pages:', 'bkb_tpl'), 'after' => '</div>')); ?>
-    </div><!-- .entry-content -->
+<div class="bkbm-entry-content">
+  <?php the_content(__('Continue reading', 'bkb_tpl')); ?>
+  <?php wp_link_pages(array('before' => '<div class="page-links">' . __('Pages:', 'bkb_tpl'), 'after' => '</div>')); ?>
+</div><!-- .entry-content -->
 
-    <?php
+<?php
 
 
     $bkb_hide_kb_nav_box = 0;
@@ -72,16 +72,18 @@ if ($bkb_single_tpl_layout == 2) {
         $bkb_kb_nav_box_title = (isset($bkb_data['bkb_kb_nav_box_title']) && $bkb_data['bkb_kb_nav_box_title'] != "" && $bkb_kb_nav_box_default_title != $bkb_data['bkb_kb_nav_box_title']) ? $bkb_data['bkb_kb_nav_box_title'] : $bkb_kb_nav_box_default_title;
 
     ?>
-        <div class="bkbm-grid bkbm-grid-pad nav-single bwl-row-cols-2">
-            <div>
-                <span class="nav-previous"><?php previous_post_link('%link', '<span class="meta-nav">' . _x('&larr;', 'Previous post link', 'bkb_tpl') . '</span> %title'); ?>&nbsp;</span>
-            </div>
-            <div class="text-right">
-                <span class="nav-next">&nbsp;<?php next_post_link('%link', '%title <span class="meta-nav">' . _x('&rarr;', 'Next post link', 'bkb_tpl') . '</span>'); ?></span>
-            </div>
-        </div><!-- .nav-single -->
+<div class="bkbm-grid bkbm-grid-pad nav-single bwl-row-cols-2">
+  <div>
+    <span
+      class="nav-previous"><?php previous_post_link('%link', '<span class="meta-nav">' . _x('&larr;', 'Previous post link', 'bkb_tpl') . '</span> %title'); ?>&nbsp;</span>
+  </div>
+  <div class="text-right">
+    <span
+      class="nav-next">&nbsp;<?php next_post_link('%link', '%title <span class="meta-nav">' . _x('&rarr;', 'Next post link', 'bkb_tpl') . '</span>'); ?></span>
+  </div>
+</div><!-- .nav-single -->
 
-    <?php
+<?php
 
     }
 
