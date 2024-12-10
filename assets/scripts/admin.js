daf271aad1afbd08d5884c32fa7214bf11516eb6
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-12-10 16:12:00 +0200

    Updated json file

--- assets/scripts/admin.js
+++ assets/scripts/admin.js
@@ -1,1 +1,134 @@
-(()=>{var t={108:()=>{var t;t=jQuery,void 0!==BkbmTplAdminData.installation&&1!=BkbmTplAdminData.installation&&t.when(t.ajax({type:"POST",url:ajaxurl,data:{action:"bkbm_tpl_installation_counter",product_id:BkbmTplAdminData.product_id},dataType:"JSON"})).done((function(t){console.log(t)}))}},a={};function r(e){var o=a[e];if(void 0!==o)return o.exports;var n=a[e]={exports:{}};return t[e](n,n.exports,r),n.exports}r.n=t=>{var a=t&&t.__esModule?()=>t.default:()=>t;return r.d(a,{a}),a},r.d=(t,a)=>{for(var e in a)r.o(a,e)&&!r.o(t,e)&&Object.defineProperty(t,e,{enumerable:!0,get:a[e]})},r.o=(t,a)=>Object.prototype.hasOwnProperty.call(t,a),(()=>{"use strict";r(108)})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/admin/modules/installation_counter.js":
+/*!***************************************************!*\
+  !*** ./src/admin/modules/installation_counter.js ***!
+  \***************************************************/
+/***/ (() => {
+
+;
+(function ($) {
+  function bkbm_tpl_installation_counter() {
+    return $.ajax({
+      type: "POST",
+      url: ajaxurl,
+      data: {
+        action: "bkbm_tpl_installation_counter",
+        // this is the name of our WP AJAX function that we'll set up next
+        product_id: BkbmTplAdminData.product_id
+      },
+      dataType: "JSON"
+    });
+  }
+  if (typeof BkbmTplAdminData.installation != "undefined" && BkbmTplAdminData.installation != 1) {
+    $.when(bkbm_tpl_installation_counter()).done(function (response_data) {
+      console.log(response_data);
+    });
+  }
+})(jQuery);
+
+/***/ }),
+
+/***/ "./src/admin/styles/admin.scss":
+/*!*************************************!*\
+  !*** ./src/admin/styles/admin.scss ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!**********************************!*\
+  !*** ./src/admin/admin_index.js ***!
+  \**********************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _styles_admin_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/admin.scss */ "./src/admin/styles/admin.scss");
+/* harmony import */ var _modules_installation_counter__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/installation_counter */ "./src/admin/modules/installation_counter.js");
+/* harmony import */ var _modules_installation_counter__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_installation_counter__WEBPACK_IMPORTED_MODULE_1__);
+// Load Stylesheets.
+
+
+// Load JavaScripts
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=admin.js.map
