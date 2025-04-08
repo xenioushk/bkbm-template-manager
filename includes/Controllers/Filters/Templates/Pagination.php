b2ecb5bdb261d30667f9b562412a292b5b3cb68e
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-08 21:51:49 +0300

    🔥 Updated: Plugin pagination helpers.

--- includes/Controllers/Filters/Templates/Pagination.php
+++ includes/Controllers/Filters/Templates/Pagination.php
@@ -21,10 +21,6 @@ class Pagination {
             return;
         }
 
-        // Load Helper File.
-
-        $this->load_helper_file();
-
         // Initialize API.
         $filters_api = new FiltersApi();
 
@@ -39,18 +35,5 @@ class Pagination {
         ];
 
         $filters_api->add_filters( $filters )->register();
-    }
-
-    /**
-     * Load the helper file.
-     *
-     * @since 1.0.0
-     */
-    public function load_helper_file() {
-
-        $file = locate_template( 'bkb_template/includes/bkbm-tpl-helpers.php' )
-			?: BKBTPL_PLUGIN_PATH . BKBTPL_TEMPLATES_DIR . 'includes/bkbm-tpl-helpers.php';
-
-			include_once $file;
     }
 }
