0359ca6055049adcaad1ddf20574570e652896af
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-08 10:00:19 +0300

    🔥 Updated: Plugin composer file. 🔥 Updated: Plugin Activate, Deactivate, Init classes.

--- bkbm-template-manager.php
+++ bkbm-template-manager.php
@@ -3,13 +3,92 @@
 /**
  * Plugin Name: Templify KB - Knowledge Base Addon
  * Plugin URI: https://1.envato.market/bkbm-wp
- * Description: Templify KB - Knowledge Base Addon allows you to display Knowledge Base categories, tags and single posts in custom templates without modifying any of the files inside theme forlder. Addon automatically handle BWL Knowledge base categories, tags and single posts templates. Addon comes with responsive and mobile friendly bkbm-grid layout. So that you can easily display you're Knowledge Base contents in small devices.
+ * Description: Addon allows you to display Knowledge Base categories, tags and single posts in custom templates without modifying any of the files inside theme forlder.
  * Author: Md Mahbub Alam Khan
  * Version: 2.0.0
  * Author URI: https://bluewindlab.net
  * WP Requires at least: 6.0+
  * Text Domain: bkb_tpl
+ * Domain Path:     /lang/
+ *
+ * @package BKBTPL
+ * @author Mahbub Alam Khan
+ * @license GPL-2.0+
+ * @link https://codecanyon.net/user/xenioushk
+ * @copyright 2025 BlueWindLab
  */
+
+
+namespace BKBTPL;
+
+// security check.
+defined( 'ABSPATH' ) || die( 'Unauthorized access' );
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+// Load the plugin constants
+if ( file_exists( __DIR__ . '/includes/Helpers/DependencyManager.php' ) ) {
+	require_once __DIR__ . '/includes/Helpers/DependencyManager.php';
+	Helpers\DependencyManager::register();
+}
+
+use KAFWPB\Base\Activate;
+use KAFWPB\Base\Deactivate;
+
+/**
+ * Function to handle the activation of the plugin.
+ *
+ * @return void
+ */
+ function activate_plugin() { // phpcs:ignore
+	$activate = new Activate();
+	$activate->activate();
+}
+
+/**
+ * Function to handle the deactivation of the plugin.
+ *
+ * @return void
+ */
+ function deactivate_plugin() { // phpcs:ignore
+	Deactivate::deactivate();
+}
+
+register_activation_hook( __FILE__, __NAMESPACE__ . '\\activate_plugin' );
+register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\deactivate_plugin' );
+
+/**
+ * Function to handle the initialization of the plugin.
+ *
+ * @return void
+ */
+function init_bkbtpl() {
+
+	// Check if the parent plugin installed.
+	if ( ! class_exists( 'BwlKbManager\\Init' ) ) {
+		add_action( 'admin_notices', [ Helpers\DependencyManager::class, 'notice_missing_main_plugin' ] );
+		return;
+	}
+
+	// Check parent plugin activation status.
+	if ( ! ( Helpers\DependencyManager::get_product_activation_status() ) ) {
+		add_action( 'admin_notices', [ Helpers\DependencyManager::class, 'notice_missing_purchase_verification' ] );
+		return;
+	}
+
+	if ( class_exists( 'BKBTPL\\Init' ) ) {
+		Init::register_services();
+	}
+}
+
+add_action( 'init', __NAMESPACE__ . '\\init_bkbtpl' );
+
+
+return;
+
+
 
 if ( ! class_exists( 'BKBM_Template_Manager' ) ) {
 
---
+++ includes/Helpers/PluginConstants.php
@@ -0,0 +1,124 @@
+<?php
+namespace BKBTPL\Helpers;
+
+/**
+ * Class for plugin constants.
+ *
+ * @package BKBTPL
+ */
+class PluginConstants {
+
+		/**
+         * Static property to hold plugin options.
+         *
+         * @var array
+         */
+	public static $plugin_options = [];
+
+	/**
+	 * Initialize the plugin options.
+	 */
+	public static function init() {
+
+		self::$plugin_options = get_option( 'bkb_options' );
+	}
+
+		/**
+         * Get the relative path to the plugin root.
+         *
+         * @return string
+         * @example wp-content/plugins/<plugin-name>/
+         */
+	public static function get_plugin_path(): string {
+		return dirname( dirname( __DIR__ ) ) . '/';
+	}
+
+
+    /**
+     * Get the plugin URL.
+     *
+     * @return string
+     * @example http://appealwp.local/wp-content/plugins/<plugin-name>/
+     */
+	public static function get_plugin_url(): string {
+		return plugin_dir_url( self::get_plugin_path() . BKBTPL_PLUGIN_ROOT_FILE );
+	}
+
+	/**
+	 * Register the plugin constants.
+	 */
+	public static function register() {
+		self::init();
+		self::set_paths_constants();
+		self::set_base_constants();
+		self::set_assets_constants();
+		self::set_updater_constants();
+		self::set_product_info_constants();
+	}
+
+	/**
+	 * Set the plugin base constants.
+     *
+	 * @example: $plugin_data = get_plugin_data( BKBTPL_PLUGIN_DIR . '/' . BKBTPL_PLUGIN_ROOT_FILE );
+	 * echo '<pre>';
+	 * print_r( $plugin_data );
+	 * echo '</pre>';
+	 * @example_param: Name,PluginURI,Description,Author,Version,AuthorURI,RequiresAtLeast,TestedUpTo,TextDomain,DomainPath
+	 */
+	private static function set_base_constants() {
+
+		$plugin_data = get_plugin_data( BKBTPL_PLUGIN_DIR . '/' . BKBTPL_PLUGIN_ROOT_FILE );
+
+		define( 'BKBTPL_PLUGIN_VERSION', $plugin_data['Version'] ?? '1.0.0' );
+		define( 'BKBTPL_PLUGIN_TITLE', $plugin_data['Name'] ?? 'Templify KB - Knowledge Base Addon' );
+		define( 'BKBTPL_TRANSLATION_DIR', $plugin_data['DomainPath'] ?? '/lang/' );
+		define( 'BKBTPL_TEXT_DOMAIN', $plugin_data['TextDomain'] ?? '' );
+
+		define( 'BKBTPL_PLUGIN_FOLDER', 'bkbm-template-manager' );
+		define( 'BKBTPL_PLUGIN_CURRENT_VERSION', BKBTPL_PLUGIN_VERSION );
+		define( 'BKBTPL_PLUGIN_POST_TYPE', 'bwl_kb' );
+		define( 'BKBTPL_PLUGIN_TAXONOMY_CAT', 'bkb_category' );
+		define( 'BKBTPL_PLUGIN_TAXONOMY_TAGS', 'bkb_tags' );
+	}
+
+	/**
+	 * Set the plugin paths constants.
+	 */
+	private static function set_paths_constants() {
+		define( 'BKBTPL_PLUGIN_ROOT_FILE', 'bkbm-template-manager.php' );
+		define( 'BKBTPL_PLUGIN_DIR', self::get_plugin_path() );
+		define( 'BKBTPL_PLUGIN_FILE_PATH', BKBTPL_PLUGIN_DIR );
+		define( 'BKBTPL_PLUGIN_URL', self::get_plugin_url() );
+	}
+
+	/**
+	 * Set the plugin assets constants.
+	 */
+	private static function set_assets_constants() {
+		define( 'BKBTPL_PLUGIN_STYLES_ASSETS_DIR', BKBTPL_PLUGIN_URL . 'assets/styles/' );
+		define( 'BKBTPL_PLUGIN_SCRIPTS_ASSETS_DIR', BKBTPL_PLUGIN_URL . 'assets/scripts/' );
+		define( 'BKBTPL_PLUGIN_LIBS_DIR', BKBTPL_PLUGIN_URL . 'libs/' );
+	}
+
+	/**
+	 * Set the updater constants.
+	 */
+	private static function set_updater_constants() {
+
+		// Only change the slug.
+		$slug        = 'bkbm/notifier_bkbm_tpl.php';
+		$updater_url = "https://projects.bluewindlab.net/wpplugin/zipped/plugins/{$slug}";
+
+		define( 'BKBTPL_PLUGIN_UPDATER_URL', $updater_url ); // phpcs:ignore
+		define( 'BKBTPL_PLUGIN_UPDATER_SLUG', BKBTPL_PLUGIN_FOLDER . '/' . BKBTPL_PLUGIN_ROOT_FILE ); // phpcs:ignore
+		define( 'BKBTPL_PLUGIN_PATH', BKBTPL_PLUGIN_DIR );
+	}
+
+	/**
+	 * Set the product info constants.
+	 */
+	private static function set_product_info_constants() {
+		define( 'BKBTPL_PRODUCT_ID', '11888104' ); // Plugin codecanyon/themeforest Id.
+		define( 'BKBTPL_PRODUCT_INSTALLATION_TAG', 'bkbm_tpl_installation_' . str_replace( '.', '_', BKBTPL_PLUGIN_VERSION ) );
+	}
+}
