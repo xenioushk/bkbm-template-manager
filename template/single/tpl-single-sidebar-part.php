16f3bf642fc4488a38c4825952f5a0ca4d682dee
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-11 19:12:43 +0300

    🔥 Updated: Plugin translation file.

--- template/single/tpl-single-sidebar-part.php
+++ template/single/tpl-single-sidebar-part.php
@@ -5,7 +5,7 @@ do_action( 'bkbm_before_sidebar_content', $bkb_single_tpl_layout );
 $sidebar = $bkb_data['bkb_single_tpl_sidebar'] ?? 'bkbm_template_widget';
 
 if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( $sidebar ) ) :
-        echo '<p>' . esc_html__( 'No widgets found in single template sidebar.', 'bkbm-template-manager' ) . '</p>';
+        echo '<p>' . esc_html__( 'No widgets found in single template sidebar.', 'bkb_tpl' ) . '</p>';
 endif;
 
 do_action( 'bkbm_after_sidebar_content', $bkb_single_tpl_layout );
--- template/tag/tpl-tag-sidebar-part.php
+++ template/tag/tpl-tag-sidebar-part.php
@@ -4,9 +4,8 @@ do_action( 'bkbm_before_sidebar_content', $bkb_tag_tpl_layout );
 
 $sidebar = $bkb_data['bkb_tag_tpl_sidebar'] ?? 'bkbm_template_widget';
 
-
 if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( $sidebar ) ) :
-        echo '<p>' . esc_html__( 'No widgets found in tag template sidebar.', 'bkbm-template-manager' ) . '</p>';
+        echo '<p>' . esc_html__( 'No widgets found in tag template sidebar.', 'bkb_tpl' ) . '</p>';
 endif;
 
 do_action( 'bkbm_after_sidebar_content', $bkb_tag_tpl_layout );
